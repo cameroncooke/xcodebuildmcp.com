1d61f5f316a97cca1885f7c356eda405d53ac6ef
Cameron Cooke <web@cameroncooke.com>
2026-02-11 10:43:34 +0000

    ✨ feat: add round favicon from project logo
    Generate circular favicon at multiple sizes (16, 32, 48, 180, 192, 512)
    with transparent backgrounds. Add favicon link tags to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
